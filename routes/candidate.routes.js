2889b2e29b78fb74ec36b63881e0acdb843f7b38
Eurico Moutinho <yungsniper@hotmail>
2022-09-14 13:32:14 +0100

    server

--- routes/candidate.routes.js
+++ routes/candidate.routes.js
@@ -19,7 +19,7 @@ router.get('/candidates', (req, res, next) => {
 });
 
 //CREATE new candidate
-router.post("/upload", [isAuthenticated, fileUploader], (req, res, next) => {
+router.post("/upload", fileUploader, (req, res, next) => {
     // console.log("file is: ", req.file)
 
     if (!req.file) {
