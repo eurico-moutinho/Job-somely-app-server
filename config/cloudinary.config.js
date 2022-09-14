855ac1ff6144ab80b9c10ba961a55491f546d87e
Eurico Moutinho <yungsniper@hotmail>
2022-09-14 16:48:40 +0100

    server

--- config/cloudinary.config.js
+++ config/cloudinary.config.js
@@ -14,22 +14,7 @@ const storage = new CloudinaryStorage({
         allowed_formats: ['jpg', 'jpeg', 'png'],
         folder: "up-work" // The name of the folder in cloudinary
 
-    }
+     }
 });
-
-function uploadFile(req, res, next) {
-    const upload = multer({ storage }).single('image');
-
-    upload(req, res, function (err) {
-        if (err) {
-
-            return res.status(400).json({
-                message: "An error occurred while uploading your image. Check your file format and size. Only jpeg, jpg and png formats allowed."
-            });
-        }
-
-        next()
-    })
-};
-
-module.exports = uploadFile;
+ 
+module.exports = multer({ storage });
