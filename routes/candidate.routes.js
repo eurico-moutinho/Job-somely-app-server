75af92f0d9f6570dbec518aef3fc99908dd8f6e9
Eurico Moutinho <yungsniper@hotmail>
2022-09-14 13:35:58 +0100

    server

--- routes/candidate.routes.js
+++ routes/candidate.routes.js
@@ -20,7 +20,7 @@ router.get('/candidates', (req, res, next) => {
 
 //CREATE new candidate
 router.post("/upload", fileUploader, (req, res, next) => {
-    // console.log("file is: ", req.file)
+    console.log("file is: ", req.file)
 
     if (!req.file) {
         res.status(400).json({ message: "No file uploaded!" });
