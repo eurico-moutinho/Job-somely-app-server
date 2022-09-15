d4fab1ecb064ad6b6d4cb6ce2b1e2f34efc922b1
Eurico Moutinho <yungsniper@hotmail>
2022-09-15 09:50:27 +0100

    server

--- config/index.js
+++ config/index.js
@@ -30,6 +30,15 @@ module.exports = (app) => {
     )
   );
 
+  app.use((req, res, next) =>{
+    res.setHeader('Access-Control-Allow-Origin', '*')
+    res.setHeader('Access-Control-Allow-Methods', 'GET,POST,PUT,DELETE') // Browser send
+    // res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization')
+    if(req.method === 'OPTIONS') {
+        return res.sendStatus(200)
+    }
+    return next()
+
   // In development environment the app logs
   app.use(logger("dev"));
   // To have access to `body` property in the request
