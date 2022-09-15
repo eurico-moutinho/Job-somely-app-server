0e488216c0579ea3d9b38dabbc777e9bd05fcdd0
Eurico Moutinho <yungsniper@hotmail>
2022-09-15 09:35:01 +0100

    server

--- config/index.js
+++ config/index.js
@@ -21,11 +21,13 @@ module.exports = (app) => {
   app.set("trust proxy", 1);
 
   app.use(
-    cors({
-      // credentials: true,
-      // origin: process.env.ORIGIN || "http://localhost:3000",
-      // optionSuccessStatus:200,
-    })
+    cors(
+    //   {
+    //   // credentials: true,
+    //   // origin: process.env.ORIGIN || "http://localhost:3000",
+    //   // optionSuccessStatus:200,
+    // }
+    )
   );
 
   // In development environment the app logs
